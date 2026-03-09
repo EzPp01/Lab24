92ab9bb65545b43a2af1e543c4cc7df69e20522a
EzPp01 <krittapat_karnna@cmu.ac.th>
2026-03-09 19:26:06 +0700

    Update lab24_1.cpp

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -1,22 +1,27 @@
 #include<iostream>
 using namespace std;
 
-
 class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
+public:
+    int data;
+    Node *next;
+    ~Node();
 };
 
 class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
+public:
+    Node *root;
+    int size;
+
+    List(int r=0,int s=0){
+        root = NULL;
+        size = 0;
+    }
+
+    void show();
+    void append(int);
+    void insert(int,int);
+    void remove(int);
 };
 
 Node::~Node(){
@@ -24,43 +29,71 @@ Node::~Node(){
 }
 
 void List::insert(int d,int idx){	
-	Node *n = new Node;
-	n->data = d;
-	
-	if(idx == 0){
-		n->next = root;
-		root = n;
-		return;
-	}
-	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
-		current = current->next;
-	}
-	n->next = current->next;
-	current->next = n;		
+    Node *n = new Node;
+    n->data = d;
+
+    if(idx == 0){
+        n->next = root;
+        root = n;
+        return;
+    }
+
+    Node *current = root;
+    for(int i = 0; i < idx-1; i++){
+        current = current->next;
+    }
+
+    n->next = current->next;
+    current->next = n;
 }
 
 void List::show(){
-	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
-		current = current->next;
-		cout << current->data << " ";
-	}	
+    Node *current = root;
+
+    while(current){
+        cout << current->data << " ";
+        current = current->next;
+    }
 }
 
 void List::append(int d){	
-	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
-		Node *current = root;
-		while(current->next){
-			current = current->next;
-		}
-		current->next = n;
-	}
-	size++;
+    Node *n = new Node;
+    n->data = d;
+    n->next = NULL;
+
+    if(root == NULL){
+        root = n;
+    }
+    else{
+        Node *current = root;
+        while(current->next){
+            current = current->next;
+        }
+        current->next = n;
+    }
+    size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx){
+    if(root == NULL) return;
+
+    Node *temp;
+
+    if(idx == 0){
+        temp = root;
+        root = root->next;
+        delete temp;
+    }
+    else{
+        Node *current = root;
+        for(int i = 0; i < idx-1; i++){
+            current = current->next;
+        }
+
+        temp = current->next;
+        current->next = temp->next;
+        delete temp;
+    }
+
+    size--;
+}
